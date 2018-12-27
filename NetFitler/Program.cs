d6f7e13926b0234edb30c36ba081352b649d02db
Pluto <Pluto@test.com>
2018-12-26 20:21:17 -0500

    update comment

--- NetFitler/Program.cs
+++ NetFitler/Program.cs
@@ -4,6 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * 
+1. Create projec
+2. Initialize folder 
+   a. change directory to the folder to be initialized
+   b. Run : git init from command line
+
+3. Add file to local git tracking
+   git --add FolderName/
+4. Commit change to local git folder
+   git commit -m"Some message"
+5. add local git folder to remote folder
+ git remote add origin https://github.com/Hubert828/NetFilter.git
+
+6. Published folder to remote git folder
+   git push original master.
+
+ */
 namespace NetFitler
 {
     class Program
